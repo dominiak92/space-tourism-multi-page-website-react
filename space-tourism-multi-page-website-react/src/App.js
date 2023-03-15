e140e71d4c9d8ad58a706e8b22823671d155e9f9
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-03-15 12:09:14 +0100

    route-fix

--- space-tourism-multi-page-website-react/src/App.js
+++ space-tourism-multi-page-website-react/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, HashRouter } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/UI/header/Header";
 import Home from "./pages/Home/Home";
 import Technology from "./pages/Technology/Technology";
@@ -8,7 +8,7 @@ import Crew from "./pages/Crew/Crew";
 
 function App() {
   return (
-    <HashRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
       </Routes>
-    </HashRouter>
+    </>
   );
 }
 
