3afa41241652d79c191021b672a00e756043e0cb
Sebastian Dominiak <sebastiandominiak92@gmail.com>
2023-03-15 10:11:19 +0100

    deploying page fix

--- space-tourism-multi-page-website-react/src/App.js
+++ space-tourism-multi-page-website-react/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/UI/header/Header";
 import Home from "./pages/Home/Home";
 import Technology from "./pages/Technology/Technology";
